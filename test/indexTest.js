c7df95cb8b47e34940c0f7a8972d23251abbe19c
eTarget1 <louisclifford10@yahoo.fr>
2019-04-11 10:55:43 -0400

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,5 +1,5 @@
 const expect = chai.expect;
-
+// const moment=require("moment")
 describe('index.js', function () {
   describe('displayTime()', function () {
     it('can successful access moment() from the momentjs node module', function () {
